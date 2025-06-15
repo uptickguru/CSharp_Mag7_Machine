91b6a8265887ba18280325a935ed7c39b5b176f5
Kevin <kevinasbury@gmail.com>
2025-06-15 16:34:07 -0400

    Add status tracking docs

---
+++ MAG7.TradingSystem/brokers/tasty/TastyTradeConfig.cs
@@ -0,0 +1,11 @@
+﻿// v1.0.1 - Warning-safe TastyTrade config class
+namespace MAG7.TradingSystem.Brokers.Tasty
+{
+    public class TastyTradeConfig
+    {
+        public required string BaseUrl { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string TokenFile { get; set; }
+    }
+}
